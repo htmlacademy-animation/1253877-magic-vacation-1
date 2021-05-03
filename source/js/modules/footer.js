453536be777d213dd5521b16929053415fdeedb9
for-animation-course <for-animation-course@yandex.ru>
2021-05-03 23:13:48 +0300

    Анимация подсказки.

--- source/js/modules/footer.js
+++ source/js/modules/footer.js
@@ -6,7 +6,12 @@ export default () => {
       footerTogglers[i].addEventListener(`click`, function () {
         let footer = footerTogglers[i].parentNode;
         if (footer.classList.contains(`screen__footer--full`)) {
-          footer.classList.remove(`screen__footer--full`);
+          footer.classList.add(`screen__footer--revert-full`);
+
+          setTimeout(() => {
+            footer.classList.remove(`screen__footer--full`);
+            footer.classList.remove(`screen__footer--revert-full`);
+          }, 400);
         } else {
           footer.classList.add(`screen__footer--full`);
         }
